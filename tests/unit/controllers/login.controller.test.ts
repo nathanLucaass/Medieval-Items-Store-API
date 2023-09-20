b6910bbc70f42b2cef3668180aa33eebb6f609e9
Nathan <nathanlucaass.silva@gmail.com>
2023-09-20 11:42:04 -0300

    some att

--- tests/unit/controllers/login.controller.test.ts
+++ tests/unit/controllers/login.controller.test.ts
@@ -3,7 +3,7 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
 import loginController from '../../../src/controller/login.controller';
-import login from '../../../src/services/login.services';
+import * as LoginService from '../../../src/services/login.services';
 
 chai.use(sinonChai);
 
@@ -34,6 +34,16 @@ describe('LoginController', function () {
     expect(res.status).to.have.been.calledWith(401);
   
     expect(res.json).to.have.been.calledWith({ message: 'Username or password invalid' });
+  });
+
+  it('should return a token if user is found', async function () {
+    req.body = { username: 'test', password: 'test' };
+    sinon.stub(LoginService, 'default').resolves({ status: 'SUCCESS', data: {token: 'token'}});
+
+    await loginController(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith({ token: 'token' });
   });
 
 });
--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -3,6 +3,7 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
 import { productRegisterController, getAllProductsController } from '../../../src/controller/product.controller';
+import * as ProductService from '../../../src/services/product.services';
 
 chai.use(sinonChai);
 
@@ -46,4 +47,36 @@ describe('productRegisterController', function () {
     expect(res.json).to.have.been.calledWith({ error: 'Some fields are missing' });
   });
 
+});
+
+describe('getAllProductsController', function () {
+  it('should return status 200 and an array of products when getAllProductsController is called', async function () {
+    const req = {} as Request;
+    const res = {} as Response;
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+
+    const products = [
+      {
+        id: 1,
+        name: 'Product 1',
+        price: '10.00',
+        orderId: 1,
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+        price: '20.00',
+        orderId: 2,
+      },
+    ]
+
+    sinon.stub(ProductService, 'getAllProducts').resolves(products);
+
+    await getAllProductsController(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(products);
+  });
 });
